f69346db89a6ec43ba76e7da66d3f8eecb8d9674
david.schnepp <david.schnepp@schneppd.com>
2017-06-23 15:49:39 +0200

    add basic locationManager

--- GeoLocationApp/app/src/main/java/lab/schneppd/geolocationapp/MainActivity.java
+++ GeoLocationApp/app/src/main/java/lab/schneppd/geolocationapp/MainActivity.java
@@ -1,13 +1,93 @@
 package lab.schneppd.geolocationapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+        }
+
+        // Acquire a reference to the system Location Manager
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
     }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        // Remove the listener you previously added
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public void Click_btnLaunchTest(View v){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
+
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        // Or use LocationManager.GPS_PROVIDER
+
+        Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        makeUseOfNewLocation(lastKnownLocation);
+
+
+
+
+
+    }
+
+    public void makeUseOfNewLocation(Location location){
+        TextView tvDebugOutput = (TextView) findViewById(R.id.tvDebugOutput);
+        if (location != null) {
+            tvDebugOutput.setText("Your location is: " + location.toString());
+        }
+        else{
+            tvDebugOutput.setText("No location found");
+        }
+    }
+
+
 }
